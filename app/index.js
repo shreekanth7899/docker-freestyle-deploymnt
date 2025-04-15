e9ff09b022e8e7b499880180c0192e1b0162a6eb
shreekanth7899 <shreekanth7899@github.com>
2025-04-15 17:45:05 +0530

    anywhere access

--- app/index.js
+++ app/index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello from Docker via Jenkins Freestyle!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
